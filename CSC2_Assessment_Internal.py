b068bb1ec2c4679a1e64bb5df8a03c3c6ccb6241
st21150WXY <st21150@ormiston.school.nz>
2022-06-09 10:47:00 +1200

    Created buttons and label widget and gridded them to its position.

--- CSC2_Assessment_Internal.py
+++ CSC2_Assessment_Internal.py
@@ -14,6 +14,19 @@ Assessment Summary Details:
 
 from tkinter import *  # This imports all the functions from tkinter module
 
+def quit():
+    pass
+
+def insert_inputs():
+    pass
+
+def print_customer_details():
+    pass
+
+def delete_row():
+    pass
+
+
 # Set up for buttons, a function
 def setup_buttons():
     # These global variables are used to define variables that are used
@@ -27,6 +40,16 @@ def setup_buttons():
     Label(main_window, text="Number of Item Hired").grid(column=0, row=2, sticky=E)
     number_of_item_hired = Entry(main_window)
     Label(main_window, text="Receipt Number")
+    customer_receipt = Entry(main_window)
+    Label(main_window, text="Row #").grid(column=3, row=2, sticky=E)
+    delete_item = Entry(main_window)
+
+    # Button Widgets
+    Button(main_window, text="Append Details", command=insert_inputs).grid(column=3, row=1)
+    Button(main_window, text="Print Details", command=print_customer_details, width=10).grid(column=4, row=1, sticky=E)
+    Button(main_window, text="Quit", command=quit, width=10).grid(column=4, row=0, sticky=E)
+    Button(main_window, text="Delete Row", command=delete_row, width=10).grid(column=4, row=3, sticky=E)
+    Label(main_window, text="               ").grid(column=2, row=0)
 
 def main():
     # These global variables are used to define variables that are used
@@ -36,7 +59,7 @@ def main():
     total_entries = 0
     # Creating the GUI and setup
     main_window = Tk()
-    #setup_buttons()
+    setup_buttons()
     main_window.mainloop()
 
 # After defining the function for main set up, the function is then called to start up the GUI
