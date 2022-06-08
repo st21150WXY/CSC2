26dc11e5c9659094e7c0c25a9c50a4fe7df90844
st21150WXY <st21150@ormiston.school.nz>
2022-06-09 11:36:35 +1200

    Created print details function alongside with an input checker only for customer name.

--- CSC2_Assessment_Internal.py
+++ CSC2_Assessment_Internal.py
@@ -18,9 +18,25 @@ from tkinter import *  # This imports all the functions from tkinter module
 def quit():
     main_window.destroy()
 
-#
+# Prints the customer details after appending it
 def print_customer_details():
-    pass
+    # Global variables that are used
+    global total_entries, name_count
+    name_count = 0
+    Label(main_window, text="Row", font='bold').grid(column=3, row=1, columnspan=1)
+    Label(main_window, text="Customer Name", font='bold').grid(column=4, row=1, columnspan=1)
+    Label(main_window, text="Item Hired", font='bold').grid(column=5, row=1, columnspan=1)
+    Label(main_window, text="Number of Item Hired", font='bold').grid(column=6, row=1, columnspan=1)
+    Label(main_window, text="Receipt Number", font='bold').grid(column=7, row=1, columnspan=1)
+
+    # Multi-Dimensional List
+    while name_count < total_entries:
+        Label(main_window, text=name_count).grid(column=3, row=name_count + 2, columnspan=1)
+        Label(main_window, text=(customer_details[name_count][0])).grid(column=4, row=name_count + 2, columnspan=1)
+        Label(main_window, text=(customer_details[name_count][1])).grid(column=5, row=name_count + 2, columnspan=1)
+        Label(main_window, text=(customer_details[name_count][2])).grid(column=6, row=name_count + 2, columnspan=1)
+        Label(main_window, text=(customer_details[name_count][3])).grid(column=7, row=name_count + 2, columnspan=1)
+        name_count += 1
 
 # This function will append all the details inputted in the entries and checks them
 def append_inputs():
@@ -45,29 +61,32 @@ def setup_buttons():
     # These global variables are used to define variables that are used
     global customer_details, customer_name, item_hired, number_of_item_hired, customer_receipt, total_entries, delete_item
 
+    # Title
+    Label(main_window, text="Julie's Tracker", font=25).grid(columnspan=5, row=0, sticky=N)
+
     # Creates an empty entry and labels along with a dropdown menu
-    Label(main_window, text="Customer Name").grid(column=0, row=0, sticky=E)
+    Label(main_window, text="Customer Name").grid(column=0, row=1, sticky=E)
     customer_name = Entry(main_window)
-    customer_name.grid(column=1, row=0)
-    Label(main_window, text="Item Hired").grid(column=0, row=1, sticky=E)
+    customer_name.grid(column=1, row=1)
+    Label(main_window, text="Item Hired").grid(column=0, row=2, sticky=E)
     item_hired = Entry(main_window)
-    item_hired.grid(column=1, row=1)
-    Label(main_window, text="Number of Item Hired").grid(column=0, row=2, sticky=E)
+    item_hired.grid(column=1, row=2)
+    Label(main_window, text="Number of Item Hired").grid(column=0, row=3, sticky=E)
     number_of_item_hired = Entry(main_window)
-    number_of_item_hired.grid(column=1, row=2)
-    Label(main_window, text="Receipt Number").grid(column=0, row=3, sticky=E)
+    number_of_item_hired.grid(column=1, row=3)
+    Label(main_window, text="Receipt Number").grid(column=0, row=4, sticky=E)
     customer_receipt = Entry(main_window)
-    customer_receipt.grid(column=1, row=3)
-    Label(main_window, text="Row #").grid(column=3, row=2, sticky=E)
+    customer_receipt.grid(column=1, row=4)
+    Label(main_window, text="Row #").grid(column=3, row=3, sticky=E)
     delete_item = Entry(main_window)
-    delete_item.grid(column=4, row=2)
+    delete_item.grid(column=4, row=3)
 
     # Button Widgets
-    Button(main_window, text="Append Details", command=append_inputs).grid(column=3, row=1)
-    Button(main_window, text="Print Details", command=print_customer_details, width=10).grid(column=4, row=1, sticky=E)
-    Button(main_window, text="Quit", command=quit, width=10).grid(column=4, row=0, sticky=E)
-    Button(main_window, text="Delete Row", command=delete_row, width=10).grid(column=4, row=3, sticky=E)
-    Label(main_window, text="               ").grid(column=2, row=0)
+    Button(main_window, text="Append Details", command=append_inputs).grid(column=3, row=2)
+    Button(main_window, text="Print Details", command=print_customer_details, width=10).grid(column=4, row=2, sticky=E)
+    Button(main_window, text="Quit", command=quit, width=10).grid(column=4, row=1, sticky=E)
+    Button(main_window, text="Delete Row", command=delete_row, width=10).grid(column=4, row=4, sticky=E)
+    Label(main_window, text="               ").grid(column=2, row=1)
 
 def main():
     # These global variables are used to define variables that are used
