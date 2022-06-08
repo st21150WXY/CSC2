e22fd55541ce90c6710b352f41645bd6c4fd6290
st21150WXY <st21150@ormiston.school.nz>
2022-06-08 14:21:02 +1200

    CSC2 Assessment

--- CSC2_Assessment_Internal.py
+++ CSC2_Assessment_Internal.py
@@ -1,3 +1,4 @@
+###/ This programs keeps track of the customer details and the number of item they have hired \###
 """
 Assessment Summary Details:
 - Customer Full Name
@@ -5,9 +6,38 @@ Assessment Summary Details:
 - Item Hired
 - Receipt Number
 - Multi-Dimensional List
-- GUI (Working Properly)
-- e
-
+- GUI (Base)
+- Button Widgets
+- Looping
+csc2
 """
 
+from tkinter import *  # This imports all the functions from tkinter module
 
+# Set up for buttons, a function
+"""def setup_buttons():
+    # These global variables are used to define variables that are used
+    global customer_details, customer_name, item_hired, number_of_item_hired, customer_receipt, total_entries, delete_item
+
+    # Creates an empty entry and labels along with a dropdown menu
+    Label(main_window, text="Customer Name").grid(column=0, row=0, sticky=E)
+    customer_name = Entry(main_window)
+    Label(main_window, text="Item Hired").grid(column=0, row=1, sticky=E)
+    item_hired = Entry(main_window)
+    Label(main_window, text="Number of Item Hired").grid(column=0, row=2, sticky=E)
+    number_of_item_hired = Entry(main_window)
+    Label(main_window, text="Receipt Number")"""
+
+def main():
+    # These global variables are used to define variables that are used
+    global main_window
+    global customer_details, customer_name, item_hired, number_of_item_hired, customer_receipt, total_entries
+    customer_details = []  # Creating an empty list
+    total_entries = 0
+    # Creating the GUI and setup
+    main_window = Tk()
+    #setup_buttons()
+    main_window.mainloop()
+
+# After defining the function for main set up, the function is then called to start up the GUI
+main()
